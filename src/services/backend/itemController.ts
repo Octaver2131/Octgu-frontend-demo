d032818abd949883174cc0398102d366da930113
Octaver <Octaver2131@outlook.com>
2025-08-15 08:36:58 +0800

    itemController.ts

---
+++ src/services/backend/itemController.ts
@@ -0,0 +1,90 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** addItem POST /api/item/add */
+export async function addItemUsingPost(body: API.ItemAddRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseInt_>('/api/item/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** deleteItem POST /api/item/delete */
+export async function deleteItemUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/item/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** updateItem POST /api/item/update */
+export async function updateItemUsingPost(
+  body: API.ItemUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/item/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** getItemById GET /api/item/get */
+export async function getItemByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: { id?: number },
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseItemVO_>('/api/item/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listItemByPage GET /api/item/list/page */
+export async function listItemByPageUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.ItemQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageItemVO_>('/api/item/list/page', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listItems GET /api/item/list */
+export async function listItemsUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.ItemQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseItemVO[]>('/api/item/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
