95f1f230fafc35a191f7793001ae02e1c3efffb7
Octaver <Octaver2131@outlook.com>
2025-08-18 09:32:13 +0800

    增加 controller 接口

--- src/services/backend/itemController.ts
+++ src/services/backend/itemController.ts
@@ -88,3 +88,26 @@ export async function listItemsUsingGet(
     ...(options || {}),
   });
 }
+
+/** getStatistics GET /api/item/statistics */
+export async function getStatisticsUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponseMapStringObject_>('/api/item/statistics', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+
+/** getMonthlyStatistics GET /api/item/monthlyStatistics */
+export async function getMonthlyStatisticsUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: { year?: number },
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseMapStringObject_>('/api/item/monthlyStatistics', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
